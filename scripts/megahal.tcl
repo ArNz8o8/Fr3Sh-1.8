ae86b5b29cca3383dfa2f9c3f45a62c30e3b0747
ArNz|8o8 <ArNz8o8@iMac-5K-van-Arnold.local>
2016-02-21 20:58:09 +0100

    Put the growing time on

--- scripts/megahal.tcl
+++ scripts/megahal.tcl
@@ -104,9 +104,9 @@ proc pub_braininfo {nick uhost hand chan arg} {
   set for [treesize -1 0]
   set back [treesize -1 1]
   puthelp "PRIVMSG $chan :My current vocabulary consists of [lindex $for 0] words, my brain size is [expr [lindex $for 1]+[lindex $back 1]] nodes, and learning mode is $learnmode" 
-#  if {[file exists megahal.old]} {
-#    puthelp "PRIVMSG $chan :This brain has been growing for [duration [expr [unixtime] - [file mtime megahal.old]]]" 
-#  }
+  if {[file exists megahal.old]} {
+    puthelp "PRIVMSG $chan :This brain has been growing for [duration [expr [unixtime] - [file mtime megahal.old]]]" 
+  }
 }
 
 bind pub n ".learningmode" pub_learningmode
